6b163706b31d2cb8e262c1259e737bc201faa572
Jehoshua J <ztxter@gmail.com>
2024-11-11 16:33:56 -0800

    Added mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,18 +16,25 @@ export default function HomePage() {
     let card1 = {
       title: "Aristocrat Connex",
       imageURL: "",
-      components: ["NodeJS", "Python", "RabbitMQ", "SocketIO"],
+      components: ["NodeJS", "Python", "RabbitMQ", "SocketIO", "AWS"],
       description: "Server that manages multiple game events in realtime and notifies numerous users of their badges that they achieved via mobile notifications"
     } as ProjectDataModel;
 
     let card2 = {
+      title: "Buffalo Big Wheel",
+      imageURL: "",
+      components: ["NodeJS", "RabbitMQ", "SocketIO", "Unity"],
+      description: "Designed architecture and game flow (e.g. game sequences and SocketIO communications). Implemented adapter for capacitive touch hardware to control Unity game"
+    } as ProjectDataModel;
+
+    let card3 = {
       title: "AWS AutoDep Tool",
       imageURL: "",
       components: ["Python", "BOTO3", "AWS"],
       description: "Internal tool used by various departments to deploy their apps onto AWS. It handled secrets, credentials, and config files for various AWS components (e.g. ElasticBeanstalk, DynamoDB, EC2, Lambda, etc.",
     } as ProjectDataModel;
 
-    const result = [card1, card2];
+    const result = [card1, card2, card3];
     return result;
   }
 
