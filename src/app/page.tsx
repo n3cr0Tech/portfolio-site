65c56e377ff25f2cc274f05a73cb816ea84fa3cf
Jehoshua J <ztxter@gmail.com>
2024-11-13 23:43:07 -0800

    disabled the password entry form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,63 +10,70 @@ import PortfolioPage from '@/pages/portfolio';
 
 export default function HomePage() {
   
-  const [password, setPassword] = useState('');
-  const [isAuthorized, setIsAuthorized] = useState(false);
-  const [error, setError] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [isAuthorized, setIsAuthorized] = useState(false);
+  // const [error, setError] = useState('');
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    setError('');  // Reset any previous error messages
+  // const handleSubmit = async (e: any) => {
+  //   e.preventDefault();
+  //   setError('');  // Reset any previous error messages
 
-    try {
-      const response = await fetch('/api/auth', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ password }),
-      });
+  //   try {
+  //     const response = await fetch('/api/auth', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({ password }),
+  //     });
 
-      if (response.ok) {
-        const data = await response.json();
-        if (data.authorized) {
-          setIsAuthorized(true);
-        } else {
-          setError('Incorrect password, please try again.');
-        }
-      } else {
-        setError('An error occurred. Please try again.');
-      }
-    } catch (err) {
-      setError('Network error. Please check your connection.');
-    }
-  };
+  //     if (response.ok) {
+  //       const data = await response.json();
+  //       if (data.authorized) {
+  //         setIsAuthorized(true);
+  //       } else {
+  //         setError('Incorrect password, please try again.');
+  //       }
+  //     } else {
+  //       setError('An error occurred. Please try again.');
+  //     }
+  //   } catch (err) {
+  //     setError('Network error. Please check your connection.');
+  //   }
+  // };
 
-  return (
+  // return (
+  //   <div>
+  //     {!isAuthorized ? (
+  //       <div className="min-h-screen flex items-center justify-center">
+  //         <form onSubmit={handleSubmit} className="p-8 bg-white shadow-md rounded">
+  //           <h1 className="text-xl font-bold mb-4 text-gray-500">Please enter the password</h1>
+  //           <input
+  //             type="password"
+  //             value={password}
+  //             onChange={(e) => setPassword(e.target.value)}
+  //             placeholder="Password"
+  //             className="border p-2 w-full text-gray-500 mb-4"
+  //           />
+  //           {error && <p className="text-red-500 mb-4">{error}</p>}
+  //           <button
+  //             type="submit"
+  //             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+  //           >
+  //             Submit
+  //           </button>
+  //         </form>
+  //       </div>
+  //     ) : (
+  //       <PortfolioPage/>
+  //     )}
+  //   </div>
+  //);
+
+  
+  return(
     <div>
-      {!isAuthorized ? (
-        <div className="min-h-screen flex items-center justify-center">
-          <form onSubmit={handleSubmit} className="p-8 bg-white shadow-md rounded">
-            <h1 className="text-xl font-bold mb-4 text-gray-500">Please enter the password</h1>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              className="border p-2 w-full text-gray-500 mb-4"
-            />
-            {error && <p className="text-red-500 mb-4">{error}</p>}
-            <button
-              type="submit"
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-            >
-              Submit
-            </button>
-          </form>
-        </div>
-      ) : (
-        <PortfolioPage/>
-      )}
+      <PortfolioPage/>
     </div>
   );
 }
