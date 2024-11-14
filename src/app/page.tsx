168b5a22262d53f082c42627dc652078bd14dbbd
Jehoshua J <ztxter@gmail.com>
2024-11-13 16:46:41 -0800

    tested some images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@
 import { ProjectDataModel } from '@/datamodels/ProjectDataModel';
 import {ProjectCard} from '@/components/ProjectCard';
 import legoPic from '@/assets/lego.jpg';
+import buffaloBigWheel from '@/assets/buffalo_big_wheel.jpg'
+import aristoConnex from '@/assets/aristocrat_connex.jpeg'
+import yoBatman from '@/assets/yo_batman.png'
+
+
 // pages/index.js
 import { useEffect, useState } from 'react';
 
@@ -23,19 +28,26 @@ export default function HomePage() {
 
     let card2 = {
       title: "Buffalo Big Wheel",
-      imageURL: legoPic,
+      imageURL: aristoConnex,
       components: ["NodeJS", "RabbitMQ", "SocketIO", "Unity"],
       description: "Designed architecture and game flow (e.g. game sequences and SocketIO communications). Implemented adapter for capacitive touch hardware to control Unity game"
     } as unknown as ProjectDataModel;
 
     let card3 = {
       title: "AWS AutoDep Tool",
-      imageURL: {},
+      imageURL: buffaloBigWheel,
       components: ["Python", "BOTO3", "AWS"],
       description: "Internal tool used by various departments to deploy their apps onto AWS. It handled secrets, credentials, and config files for various AWS components (e.g. ElasticBeanstalk, DynamoDB, EC2, Lambda, etc.",
     } as ProjectDataModel;
 
-    const result = [card1, card2, card3];
+    let card4 = {
+      title: "Yo Batman",
+      imageURL: yoBatman,
+      components: ["Python"],
+      description: "Internal tool used by various departments to deploy their apps onto AWS. It handled secrets, credentials, and config files for various AWS components (e.g. ElasticBeanstalk, DynamoDB, EC2, Lambda, etc.",
+    } as ProjectDataModel;
+
+    const result = [card1, card4, card1, card2, card3, ];
     return result;
   }
 
