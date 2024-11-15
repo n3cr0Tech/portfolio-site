cc047412d18542f818091059acf2f3c8ee336174
Jehoshua J <ztxter@gmail.com>
2024-11-15 14:59:15 -0800

    depracated ensureImageDisplay(), using Carousel component. TODO: if there's no image for a card, there shouldnt be a gap between the description and components

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,25 +1,26 @@
 
 import {ProjectDataModel} from "../datamodels/ProjectDataModel";
 import Image, { StaticImageData } from "next/image";
+import { Carousel } from "./Carousel";
 
 export const ProjectCard = (props: {projectCards: ProjectDataModel[]}) =>{
 
-    const ensureImageDisplay = (imgUrl: StaticImageData) => {    
-        if (imgUrl.src != null){            
-            return (
-                <Image
-                    src = {imgUrl}
-                    alt = "img"
-                    width = {180}
-                    height = {38}
-                    layout = "responsive"
-                    priority
-                />
-            )       
-        }else{
-            return <p></p>
-        }
-    }
+    // const ensureImageDisplay = (imgUrl: StaticImageData) => {    
+    //     if (imgUrl.src != null){            
+    //         return (
+    //             <Image
+    //                 src = {imgUrl}
+    //                 alt = "img"
+    //                 width = {180}
+    //                 height = {38}
+    //                 layout = "responsive"
+    //                 priority
+    //             />
+    //         )       
+    //     }else{
+    //         return <p></p>
+    //     }
+    // }
 
     return (
         <div className="flex flex-col space-y-6 w-full max-w-2xl">
@@ -27,7 +28,10 @@ export const ProjectCard = (props: {projectCards: ProjectDataModel[]}) =>{
                 <div key={index} className="bg-white rounded-lg shadow-md overflow-hidden w-full">
                     <h2 className="text-xl font-semibold p-4 text-black">{e.title}</h2>                    
                     <div className="flex justify-center items-center max-h-[300px] overflow-hidden">
-                        {ensureImageDisplay(e.imageURL)}
+                        {/* {ensureImageDisplay(e.imageURL)} */}
+                        <div className="relative w-full h-64 overflow-hidden">
+                            <Carousel images={e.imageURLs}/>
+                        </div>
                     </div>
                     <p className="p-4 text-black">{e.description}</p>                    
                     <div className="p-4 bg-gray-500 text-white text-md p-2">
