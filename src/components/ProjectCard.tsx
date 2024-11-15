553a52a97edbd0a747736ca7f17bceafe98edabf
Jehoshua J <ztxter@gmail.com>
2024-11-15 15:10:39 -0800

    BUG FIX: if a card doesnt have any images to show, there wouldnt be a gap between the title and the components

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -5,33 +5,23 @@ import { Carousel } from "./Carousel";
 
 export const ProjectCard = (props: {projectCards: ProjectDataModel[]}) =>{
 
-    // const ensureImageDisplay = (imgUrl: StaticImageData) => {    
-    //     if (imgUrl.src != null){            
-    //         return (
-    //             <Image
-    //                 src = {imgUrl}
-    //                 alt = "img"
-    //                 width = {180}
-    //                 height = {38}
-    //                 layout = "responsive"
-    //                 priority
-    //             />
-    //         )       
-    //     }else{
-    //         return <p></p>
-    //     }
-    // }
+    const ensureImageDisplay = (imgUrls: StaticImageData[]) => {    
+        if (imgUrls.length > 0){            
+            return (
+                <div className="relative w-full h-64 overflow-hidden">
+                    <Carousel images={imgUrls}/>
+                </div>                
+            )       
+        }
+    }
 
     return (
         <div className="flex flex-col space-y-6 w-full max-w-2xl">
             {props.projectCards.map((e, index) => (
                 <div key={index} className="bg-white rounded-lg shadow-md overflow-hidden w-full">
                     <h2 className="text-xl font-semibold p-4 text-black">{e.title}</h2>                    
-                    <div className="flex justify-center items-center max-h-[300px] overflow-hidden">
-                        {/* {ensureImageDisplay(e.imageURL)} */}
-                        <div className="relative w-full h-64 overflow-hidden">
-                            <Carousel images={e.imageURLs}/>
-                        </div>
+                    <div className="flex justify-center items-center max-h-[300px] overflow-hidden">                        
+                        {ensureImageDisplay(e.imageURLs)}
                     </div>
                     <p className="p-4 text-black">{e.description}</p>                    
                     <div className="p-4 bg-gray-500 text-white text-md p-2">
